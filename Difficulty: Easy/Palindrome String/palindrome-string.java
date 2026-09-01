75b111c3b7a90416644cbaee4a8501fabf006d8c
Mutyala Indira Mallarapu <mutyalaindiramallarapu@gmail.com>
2026-09-01 13:58:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.java
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -1,7 +1,8 @@
 class Solution {
     boolean isPalindrome(String s) {
         // code here
-        int i=0,j=s.length()-1;
+        int n=s.length();
+        int i=0,j=n-1;
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)) return false;
             i++;
